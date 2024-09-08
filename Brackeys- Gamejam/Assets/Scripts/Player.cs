e5c2cf079a8505c91bbd6045dd618afd53b62af2
Jiho Lee <61943484+jihojiho22@users.noreply.github.com>
2024-09-08 19:08:23 -0400

    added jump to player

--- Brackeys- Gamejam/Assets/Scripts/Player.cs
+++ Brackeys- Gamejam/Assets/Scripts/Player.cs
@@ -2,20 +2,45 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f; 
+    public float moveSpeed = 5f;
+    public float jumpPower = 5f;
     private Rigidbody rb;
+    private bool isGrounded;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>(); 
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+         if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+        }
     }
 
     void FixedUpdate()
     {
-     
-        float moveHorizontal = Input.GetAxis("Horizontal"); 
-        float moveVertical = Input.GetAxis("Vertical"); 
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical) * moveSpeed * Time.fixedDeltaTime;
         rb.MovePosition(rb.position + movement);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
     }
 }
